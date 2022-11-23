9ddc4a678b7d23c4993471a87e79e1cb600f7a74
tomasAtrat <tomasatrat@gmail.com>
2022-11-23 00:15:43 -0300

    changes 4 deployment

--- src/main/java/com/smartstore/smartstorewebservice/SmartStoreWebServiceApplication.java
+++ src/main/java/com/smartstore/smartstorewebservice/SmartStoreWebServiceApplication.java
@@ -2,13 +2,19 @@ package com.smartstore.smartstorewebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class SmartStoreWebServiceApplication {
+public class SmartStoreWebServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartStoreWebServiceApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SmartStoreWebServiceApplication.class);
+    }
 }
